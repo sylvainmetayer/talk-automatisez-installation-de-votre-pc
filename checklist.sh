5a63cefe940ff4cfc427aba50baf505205d5598e
Sylvain METAYER <s.metayer@groupeonepoint.com>
2023-06-29 08:02:08 +0200

    update checklist

--- checklist.sh
+++ checklist.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "- Mode Ne Pas Deranger actif ?"
+echo "- T'as un backup VM fini au cas ou ?"
+echo "- Kill docker + cbagentd pour sauver de la batterie"
 echo "- La VM est reset ?"
 echo "- Les slides sont disponible en local au cas ou ?"
 echo "- Open presentation mode on local intellij for better readability and open 'dotfiles/README.md'"
